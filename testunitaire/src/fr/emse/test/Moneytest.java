1f5dcb5264d0362c9f605d21f5373fa72baba433
Houlder <Houlder@DESKTOP-PCRDHOO>
2024-04-03 06:31:43 +0300

    1er test unitaire

---
+++ testunitaire/src/fr/emse/test/Moneytest.java
@@ -0,0 +1,28 @@
+package fr.emse.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Moneytest {
+
+	
+	@Test
+	public void testSimpleAdd() {
+	Money m12CHF = new Money(12, "CHF");
+	Money m14CHF = new Money(14, "CHF");
+	Money expected = new Money(26, "CHF");
+	Money result = m12CHF.add(m14CHF); 
+	assertTrue(expected.equals(result));
+
+  }
+	@Test
+	public void testEquals() {
+	Money m12CHF= new Money(12, "CHF");
+	Money m14CHF= new Money(14, "CHF");
+	assertTrue(!m12CHF.equals(null));
+	assertEquals(m12CHF, m12CHF);
+	assertEquals(m12CHF, new Money(12, "CHF"));
+	assertTrue(!m12CHF.equals(m14CHF));
+	}
+}
