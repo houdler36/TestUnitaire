24b580075d1e6f5f9fecc6c1cc2fec92389da688
Houlder <Houlder@DESKTOP-PCRDHOO>
2024-04-03 07:10:04 +0300

    3eme commite

--- testunitaire/src/fr/emse/test/Moneytest.java
+++ testunitaire/src/fr/emse/test/Moneytest.java
@@ -2,15 +2,24 @@ package fr.emse.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Moneytest {
-
+	
+	private Money m12CHF;
+	private Money m14CHF;
+	
+	@Before
+	
+	public void setUp() {
+		m12CHF = new Money(12, "CHF");
+		m14CHF = new Money(14, "CHF");
+	}
 	
 	@Test
 	public void testSimpleAdd() {
-	Money m12CHF = new Money(12, "CHF");
-	Money m14CHF = new Money(14, "CHF");
+	
 	Money expected = new Money(26, "CHF");
 	Money result = m12CHF.add(m14CHF); 
 	assertTrue(expected.equals(result));
@@ -18,8 +27,7 @@ public class Moneytest {
   }
 	@Test
 	public void testEquals() {
-	Money m12CHF= new Money(12, "CHF");
-	Money m14CHF= new Money(14, "CHF");
+	
 	assertTrue(!m12CHF.equals(null));
 	assertEquals(m12CHF, m12CHF);
 	assertEquals(m12CHF, new Money(12, "CHF"));
